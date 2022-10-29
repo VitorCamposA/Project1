532935c6ca9cb7532417f1917617017f526e8ee6
Vitann <projectvnn@DESKTOP-JOMJKIR.localdomain>
2022-10-28 21:30:46 -0300

    segundo save aprendizado php

--- trying.php
+++ trying.php
@@ -1,5 +1,18 @@
 <?php
+
 echo "Eu escrevo algo <br/> ";
-print "Eu também escrevo algo";
-$var = "Eu decido meu tipo";
+print "Eu também escrevo algo<br/>";
+
+$var = "Eu decido meu tipo <br?> ";
+$varn = 4;
+echo gettype($var); //mostra o tipo de variavel
+
+if($varn == 3) {
+    echo "certo";
+}
+else{
+    echo "errado";
+}
+
+
 ?>
